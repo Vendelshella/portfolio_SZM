9e19025d55a2ded0d74b8086b294d92254b61458
Sonia Zamora Martín <hechodeoroverde@gmail.com>
2023-09-20 10:49:59 +0200

    cambios css proyectos index.html

--- js/index.js
+++ js/index.js
@@ -5,16 +5,21 @@ $(document).ready(()=>{
 
     $("#logo_deoroverde").mouseenter(()=>{
         $("#text_deoroverde").stop().fadeIn(1000);
+        $("#logo_deoroverde").stop().css({backgroundColor:"rgb(127, 197, 197)"});
     })
     $("#logo_deoroverde").mouseleave(() => {
         $("#text_deoroverde").stop().fadeOut(1000);
+        $("#logo_deoroverde").stop().css({backgroundColor:""});
     });
 
     $("#logo_starfinder").mouseenter(()=>{
         $("#text_starfinder").stop().fadeIn(1000);
+        $("#logo_starfinder").stop().css({backgroundColor:"rgb(127, 197, 197)"});
+
     })
     $("#logo_starfinder").mouseleave(() => {
         $("#text_starfinder").stop().fadeOut(1000);
+        $("#logo_starfinder").stop().css({backgroundColor:""});
     });
 
 })
